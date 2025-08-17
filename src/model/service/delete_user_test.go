8b683e4711fdfaa79eefd30aa79e1c08faf4d8fe
Jordan <jordan.smta@gmail.com>
2025-08-17 10:05:03 -0300

    chore: criando testes unitarios - camada service

---
+++ src/model/service/delete_user_test.go
@@ -0,0 +1,41 @@
+package service
+
+import (
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	rest_err "github.com/jordanmarta/go-crud-starter/src/configuration"
+	"github.com/jordanmarta/go-crud-starter/src/tests/mocks"
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func TestUserDomainService_DeleteUser(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	repository := mocks.NewMockUserRepository(ctrl)
+	service := NewUserDomainService(repository)
+
+	t.Run("when_sending_a_valid_userId_returns_success", func(t *testing.T) {
+		id := primitive.NewObjectID().Hex()
+
+		repository.EXPECT().DeleteUser(id).Return(nil)
+
+		err := service.DeleteUser(id)
+
+		assert.Nil(t, err)
+	})
+
+	t.Run("when_sending_a_invalid_userId_returns_error", func(t *testing.T) {
+		id := primitive.NewObjectID().Hex()
+
+		repository.EXPECT().DeleteUser(id).Return(
+			rest_err.NewInternalServerError("error trying to update user"))
+
+		err := service.DeleteUser(id)
+
+		assert.NotNil(t, err)
+		assert.EqualValues(t, err.Message, "error trying to update user")
+	})
+}
